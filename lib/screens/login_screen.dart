d61b41dd2bededa5092aa6be354ff0501770166a
Edward Maeng <emaeng@vmware.com>
2022-11-15 07:00:50 +0000

    Developed Google Docs

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_docs/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_docs/repository/auth_repository.dart';
+import 'package:routemaster/routemaster.dart';
+
+class LoginScreen extends ConsumerWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  void signInWithGoogle(WidgetRef ref, BuildContext context) async {
+    final sMessenger = ScaffoldMessenger.of(context);
+    final navigator = Routemaster.of(context);
+    final errorModel =
+        await ref.read(authRepositoryProvider).signInWithGoogle();
+    if (errorModel.error == null) {
+      ref.read(userProvider.notifier).update((state) => errorModel.data);
+      navigator.replace('/');
+    } else {
+      sMessenger.showSnackBar(
+        SnackBar(
+          content: Text(errorModel.error!),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton.icon(
+          onPressed: () => signInWithGoogle(ref, context),
+          icon: Image.asset(
+            'assets/images/g-logo.png',
+            height: 20,
+          ),
+          label: const Text(
+            'Sign in with Google',
+            style: TextStyle(
+              color: kBlackColor,
+            ),
+          ),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kWhiteColor,
+            minimumSize: const Size(150, 50),
+          ),
+        ),
+      ),
+    );
+  }
+}
